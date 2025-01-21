2425542ff6c0226a26362209ba8925a34fea2a2f
Tamir <udelgombotamira@gmail.comqqgit config user.email udelgombotamira@gmail.com>
2025-01-21 15:21:38 +0800

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,86 +1,63 @@
-<div class="w-full lg:h-72 bg-primary-950 flex flex-col overflow-x-auto px-10 md:px-20 lg:px-32">
-      <div class="w-full flex justify-between py-8 ">
+<div class="w-full lg:h-72 bg-white flex flex-col overflow-x-auto px-10 md:px-20 lg:px-32 border-t-2 border-gray-150">
+    <div class="w-full flex justify-between py-8 ">
         <div class="flex lg:gap-0 gap-8">
-          <div class="pt-5 text-white text-xs ">
-            <div class="opacity-30 pb-5">Цэсүүд</div>
-            <div class="grid grid-col-4 gap-2">
-              <a href="/">Нүүр</a>
-              <a href="/page/proger-gej-yu-ve">
-                Прогэр гэж юу вэ?
-              </a>
-              <a href="/page/uilchilgee">
-                Үйлчилгээ
-              </a>
-              <a href="/page/chansaa">
-                Чансаа
-              </a>
+            <div class="pt-5 text-black text-xs ">
+                <div class="opacity-30 pb-5">Цэсүүд</div>
+                <div class="grid grid-col-4 gap-2">
+                    <a href="/" class="text-black">Нүүр</a>
+                    <a href="/page/proger-gej-yu-ve" class="text-black">
+                    </a>
+                    <a href="/page/uilchilgee" class="text-black">
+                        Үйлчилгээ
+                    </a>
+                    <a href="/page/chansaa" class="text-black">
+                        Чансаа
+                    </a>
+                </div>
             </div>
-          </div>
-          <div class="text-white lg:ml-40 pt-5 text-xs w-48 h-20">
-            <div class=" opacity-30 pb-5">Хаяг</div>
-            <div>
-              Монгол Улс, Улаанбаатар хот, Сүхбаатар дүүрэг, 8-хороо Амарын
-              гудамж-29 Сан Бизнес Центр, 402 тоот. 
-              <div class="mt-3">
-                <p>Email: <a href="mailto:contact@legaldata.mn" class="hover:text-pink-500">contact@legaldata.mn</a></p>
-                <p>Утас: 99915239</p>
-              </div>
-              
+            <div class="text-black lg:ml-40 pt-5 text-xs w-48 h-20">
+                <div class=" opacity-30 pb-5">Хаяг</div>
+                <div>
+                    Монгол Улс, Улаанбаатар хот, Сүхбаатар дүүрэг, 8-хороо Амарын
+                    гудамж-29 Сан Бизнес Центр, 402 тоот.
+                    <div class="mt-3">
+                        <p>Email: <a href="mailto:contact@legaldata.mn"
+                                class="hover:text-pink-500 text-black">contact@legaldata.mn</a></p>
+                        <p>Утас: 99915239</p>
+                    </div>
+
+                </div>
             </div>
-          </div>
         </div>
         <div class="h-full flex items-end">
-          <img
-            src="/proger-logo5.png"
-            class="h-20 hidden md:block"
-            alt="Proger"
-          />
+            <img src="/LDLogo2.png" class="h-20 hidden md:block" alt="Proger" />
         </div>
-      </div>
-      <div class="h-[1px] w-full bg-[#525984]"></div>
-      <div class="w-full flex justify-between lg:flex-row flex-col">
-        <div class="mt-5 text-white flex text-sm">
-          <div> 
-            <p class="mb-1">
-              <span>
-                <a href="/page/term-of-service" class="hover:text-pink-500">Үйлчилгээний нөхцөл </a>
-              </span>
-              <span class="ml-3">
-                <a href="/page/privacy-policy" class="hover:text-pink-500">Нууцлалын журам, бодлого</a>
-              </span>
-              
-            </p>
-             © 2022-2023. Легал дата ХХК. All Rights Reserved
-          </div>
+    </div>
+    <div class="h-[1px] w-full bg-[#525984]"></div>
+    <div class="w-full flex justify-between lg:flex-row flex-col">
+        <div class="mt-5 text-black flex text-sm">
+            <div>
+                <p class="mb-1">
+                    <span>
+                        <a href="/page/term-of-service" class="hover:text-pink-500 text-black">Үйлчилгээний нөхцөл </a>
+                    </span>
+                    <span class="ml-3">
+                        <a href="/page/privacy-policy" class="hover:text-pink-500 text-black">Нууцлалын журам,
+                            бодлого</a>
+                    </span>
+
+                </p>
+                © 2022-2023. Легал дата ХХК. All Rights Reserved
+            </div>
         </div>
-        <div> 
-          
+        <div>
+
         </div>
         <div class="flex gap-2 py-5">
-          <img
-            src="/images/social/facebook.svg"
-            class=" w-5 h-5"
-            alt="facebook"
-          />
-          <img
-            src="/images/social/instagram.svg"
-            class=" w-5 h-5"
-            alt="instagram"
-          />
-          <img
-            src="/images/social/youtube.svg"
-            class=" w-5 h-5"
-            alt="youtube"
-            width={100}
-            height={80}
-          />
-          <img
-            src="/images/social/twitter.svg"
-            class=" w-5 h-5"
-            alt="twitter"
-            width={100}
-            height={80}
-          />
+            <img src="/images/social/facebook.svg" class=" w-5 h-5" alt="facebook" />
+            <img src="/images/social/instagram.svg" class=" w-5 h-5" alt="instagram" />
+            <img src="/images/social/youtube.svg" class=" w-5 h-5" alt="youtube" width={100} height={80} />
+            <img src="/images/social/twitter.svg" class=" w-5 h-5" alt="twitter" width={100} height={80} />
         </div>
-      </div>
+    </div>
 </div>
